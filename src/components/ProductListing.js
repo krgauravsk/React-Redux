59b88fba333d055939636be03abbada289ad8dac
Kumar Gaurav <i_kumargaurav@lidolearning.com>
2021-07-11 11:30:30 +0530

    allProduct api-link

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from "axios";
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import ProductComponent from "./ProductComponent";
+import { setProducts } from "../redux/actions/productActions";
 
 const ProductListing = () => {
+    const products = useSelector((state) => state.allProducts.product);
+    const dispatch = useDispatch();
+
+    //api-link for allProduct-setup
+    const fetchProduct = async () => {
+        const response = await axios.get("'https://fakestoreapi.com/products")
+            .catch((err) => {
+                console.log("Error: ", err);
+            });
+        dispatch(setProducts(response));
+    };
+    useEffect(() => {
+        fetchProduct();
+    });
+    console.log("Products:", products);
+
+
     return (
         <div>
-            <h2>list</h2>
+            <h2><ProductComponent /></h2>
         </div>
     )
 }
